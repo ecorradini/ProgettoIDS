cf5714eafe0323c988df9bd16e0031c1f2ef169d
alessandromentuccia <alessandro.mentuccia@hotmail.it>
2018-02-01 16:21:56 +0100

    creazione package

--- AndroidStudioProject/app/src/main/java/it/getout/GestioneConnessioni/DBHelper.java
+++ AndroidStudioProject/app/src/main/java/it/getout/GestioneConnessioni/DBHelper.java
@@ -1,8 +1,25 @@
 package it.getout.GestioneConnessioni;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
+
 /**
  * Created by Alessandro on 01/02/2018.
  */
 
-public class DBHelper {
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+
+    }
+
 }
--- AndroidStudioProject/app/src/main/java/it/getout/GestioneVisualizzazioneMappa/MappaView.java
+++ AndroidStudioProject/app/src/main/java/it/getout/GestioneVisualizzazioneMappa/MappaView.java
@@ -1,10 +1,14 @@
 package it.getout.GestioneVisualizzazioneMappa;
 
+import android.content.Context;
 import android.widget.ImageView;
 
 /**
  * Created by Alessandro on 01/02/2018.
  */
 
-public class MappaView {
-        }
+public class MappaView extends android.support.v7.widget.AppCompatImageView{
+    public MappaView(Context context) {
+        super(context);
+    }
+}
