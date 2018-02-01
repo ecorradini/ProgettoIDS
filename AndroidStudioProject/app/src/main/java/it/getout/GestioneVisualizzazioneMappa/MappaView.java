120743dd5aee3bdd7afbc523325600b7e06a03f0
baldo04 <baldo.94@hotmail.it>
2018-02-01 16:47:32 +0100

    gestione visualizzazione mappa finito

--- AndroidStudioProject/app/src/main/java/it/getout/GestioneVisualizzazioneMappa/MappaView.java
+++ AndroidStudioProject/app/src/main/java/it/getout/GestioneVisualizzazioneMappa/MappaView.java
@@ -2,7 +2,11 @@ package it.getout.GestioneVisualizzazioneMappa;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Point;
+import android.util.AttributeSet;
 import android.widget.ImageView;
+
+import it.getout.GestionePosizione.Percorso;
 
 /**
  * Created by Alessandro on 01/02/2018.
@@ -15,6 +19,19 @@ public class MappaView extends android.support.v7.widget.AppCompatImageView{
 
     private Canvas canvas;
 
+    public void MappaView(Context context, AttributeSet attributeSet){   // costruttore non sicuro del void
+    }
 
+    protected void onDraw(Canvas canvas) {
+
+    }
+
+    public void disegnaPercorso(Canvas canvas, Percorso percorso) {
+
+    }
+
+    public void disegnaPosizione(Canvas canvas, Point point) {
+
+    }
 
 }
