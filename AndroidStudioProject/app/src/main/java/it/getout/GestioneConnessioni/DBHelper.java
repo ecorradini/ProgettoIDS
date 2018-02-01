db1f2c73dbc589cd3cc70abd920a946c69b860bf
baldo04 <baldo.94@hotmail.it>
2018-02-01 17:07:52 +0100

    gestione visualizzazione mappa finito

--- AndroidStudioProject/app/src/main/java/it/getout/GestioneConnessioni/DBHelper.java
+++ AndroidStudioProject/app/src/main/java/it/getout/GestioneConnessioni/DBHelper.java
@@ -4,12 +4,21 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Bitmap;
+import android.graphics.Point;
+
+import java.util.ArrayList;
+
+import it.getout.GestionePosizione.Edificio;
+import it.getout.GestionePosizione.Piano;
 
 /**
  * Created by Alessandro on 01/02/2018.
  */
 
 public class DBHelper extends SQLiteOpenHelper {
+
+    private int DATABASE_VERSION;
+
     public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
@@ -21,5 +30,24 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
 
     }
+
+
+    public Edificio initEdificioAttuale(String string) {   //tipo edificio ancora non è dato sapere
+            return new Edificio();
+    }
+
+    public Piano initPianoAttuale(Edificio edificio){
+            return new Piano();
+    }
+
+    public Point getPosizione(String string) {
+        return new Point();                     //fatto a caso
+    }
+
+    public ArrayList<Piano> initPiani(String string) {
+        return new ArrayList();                 //fatto a caso
+    }
+
+
 
 }
--- AndroidStudioProject/app/src/main/java/it/getout/GestioneConnessioni/ServerHelper.java
+++ AndroidStudioProject/app/src/main/java/it/getout/GestioneConnessioni/ServerHelper.java
@@ -1,8 +1,13 @@
 package it.getout.GestioneConnessioni;
 
+import android.graphics.Point;
+
 /**
  * Created by Alessandro on 01/02/2018.
  */
 
 public class ServerHelper {
+    public static void richiediPercorso(Point point1, Point point2) {
+
+    }
 }
