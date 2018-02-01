d39f5d61501c6bed873026eeffcc346eface8b38
baldo04 <baldo.94@hotmail.it>
2018-02-01 16:37:41 +0100

    gestione visualizzazione mappa

--- AndroidStudioProject/app/src/main/java/it/getout/GestioneVisualizzazioneMappa/Mappa.java
+++ AndroidStudioProject/app/src/main/java/it/getout/GestioneVisualizzazioneMappa/Mappa.java
@@ -1,8 +1,36 @@
 package it.getout.GestioneVisualizzazioneMappa;
 
+import android.graphics.Bitmap;
+
 /**
  * Created by Alessandro on 01/02/2018.
  */
 
 public class Mappa {
+    private Bitmap immagine;
+    private double witdh;
+    private double height;
+
+    public double getWitdh() {
+        return witdh;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setMappa (String string) {
+    }
+
+    public void getMappaDB(String string){
+    }
+
+    public Bitmap getMappa() {
+        return immagine;            //ritorno cosa? messo a caso
+    }
+
+    public String convertiDaBase64(String string) {
+        return string;              //ritorno cosa? messo a caso
+    }
+
 }
