e8cd0a742a9f10fac1c4106d15197e38efdfb147
alessandromentuccia <alessandro.mentuccia@hotmail.it>
2018-02-01 16:52:09 +0100

    creazione package

--- AndroidStudioProject/app/src/main/java/it/getout/GestionePosizione/Edificio.java
+++ AndroidStudioProject/app/src/main/java/it/getout/GestionePosizione/Edificio.java
@@ -1,8 +1,29 @@
 package it.getout.GestionePosizione;
 
+import java.util.ArrayList;
+
 /**
  * Created by Alessandro on 01/02/2018.
  */
 
 class Edificio {
+
+    String nome;
+    ArrayList<Piano> piani;
+
+    Edificio(String name){
+
+    }
+
+    Piano getPiano(int index){
+        return piani.get(index);
+    }
+
+    void getPianiDB(){
+
+    }
+
+    //ArrayList<Piano> getPiani(){
+
+    //}
 }
