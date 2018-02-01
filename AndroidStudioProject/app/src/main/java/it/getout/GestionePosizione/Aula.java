12a021242e18c1b325a7b12d35da176f2c06918c
alessandromentuccia <alessandro.mentuccia@hotmail.it>
2018-02-01 16:05:47 +0100

    creazione package

---
+++ AndroidStudioProject/app/src/main/java/it/getout/GestionePosizione/Aula.java
@@ -0,0 +1,8 @@
+package it.getout.GestionePosizione;
+
+/**
+ * Created by Alessandro on 01/02/2018.
+ */
+
+class Aula {
+}
---
+++ AndroidStudioProject/app/src/main/java/it/getout/GestioneVisualizzazioneMappa/MappaView.java
@@ -0,0 +1,10 @@
+package it.getout.GestioneVisualizzazioneMappa;
+
+import android.widget.ImageView;
+
+/**
+ * Created by Alessandro on 01/02/2018.
+ */
+
+public class MappaView {
+        }
