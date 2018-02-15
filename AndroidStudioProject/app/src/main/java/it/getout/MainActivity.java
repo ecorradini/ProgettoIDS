e00782ee85727d81a32e2b012adb2aff0f75cbb7
alessandromentuccia <alessandro.mentuccia@hotmail.it>
2018-02-15 17:59:27 +0100

    sistemazione classi di package gestioneposizione

--- AndroidStudioProject/app/src/main/java/it/getout/MainActivity.java
+++ AndroidStudioProject/app/src/main/java/it/getout/MainActivity.java
@@ -1,13 +1,16 @@
 package it.getout;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.ParcelUuid;
-import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.Set;
 
 import it.getout.gestioneposizione.PosizioneUtente;
 
@@ -15,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
 
     private BluetoothAdapter mBluetoothAdapter = null;
     private PosizioneUtente posUtente;
-
+    private Set dispositivi;
+    private static final int BLUETOOTH_ON=1000;
+    private ArrayAdapter adapter = null;
     public static final ParcelUuid Service_UUID = ParcelUuid.fromString("0000b81d-0000-1000-8000-00805f9b34fb");
     public static final int REQUEST_ENABLE_BT = 1;
 
@@ -45,4 +50,20 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+   /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==BLUETOOTH_ON && resultCode==RESULT_OK)
+        {
+            load();
+        }
+    }
+
+    private void load() {
+        dispositivi = mBluetoothAdapter.getBondedDevices();
+        adapter.clear();
+        for(BluetoothDevice bt : dispositivi)
+            adapter.add(bt.getName());
+    }*/
 }
