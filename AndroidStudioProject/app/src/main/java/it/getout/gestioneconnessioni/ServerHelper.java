c5513a2c548f410aa1ce453cabd6c4a7df3802a3
ecorradini <birillo3000#>
2018-02-20 17:08:46 +0100

    edo merda

--- AndroidStudioProject/app/src/main/java/it/getout/gestioneconnessioni/ServerHelper.java
+++ AndroidStudioProject/app/src/main/java/it/getout/gestioneconnessioni/ServerHelper.java
@@ -1,13 +1,34 @@
 package it.getout.gestioneconnessioni;
 
 import android.graphics.Point;
+import android.graphics.PointF;
+import android.os.AsyncTask;
 
 /**
  * Created by Alessandro on 01/02/2018.
  */
 
 public class ServerHelper {
-    public static void richiediPercorso(Point point1, Point point2) {
+    //AsyncTask che richiede il percorso al Server
+    private class richiediPercorso extends AsyncTask<PointF[],Void,Boolean> {
+        @Override
+        protected void onPreExecute() {
 
+        }
+
+        @Override
+        protected Boolean doInBackground(PointF[]... points) {
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if(result) {
+                //ho il percorso del server
+            }
+            else {
+                //errore download
+            }
+        }
     }
 }
--- AndroidStudioProject/app/src/main/java/it/getout/gestioneposizione/Beacon.java
+++ AndroidStudioProject/app/src/main/java/it/getout/gestioneposizione/Beacon.java
@@ -1,6 +1,7 @@
 package it.getout.gestioneposizione;
 
 import android.graphics.Point;
+import android.graphics.PointF;
 
 /**
  * Created by Alessandro on 01/02/2018.
@@ -8,14 +9,14 @@ import android.graphics.Point;
 
 public class Beacon {
     private String id;
-    private Point posizione;
+    private PointF posizione;
 
-    public Beacon(String id, Point posizione){
+    public Beacon(String id, PointF posizione){
         this.id = id;
         this.posizione = posizione;
     }
 
-    public Point getPosizione(){
+    public PointF getPosizione(){
         return posizione;
     }
 
