07c1a1cd98ff2ce0c2ad4f07b73b093c51772c8d
ecorradini <birillo3000#>
2018-02-20 18:14:05 +0100

    edo merda

--- AndroidStudioProject/app/src/main/java/it/getout/gestioneposizione/PosizioneUtente.java
+++ AndroidStudioProject/app/src/main/java/it/getout/gestioneposizione/PosizioneUtente.java
@@ -30,17 +30,24 @@ public class PosizioneUtente {
     private static Edificio edificioAttuale;
     //Istanza del piano in cui si trova l'utente
     private static Piano pianoAttuale;
+    //Beacon al quale l'utente è collegato al momento
+    private static Beacon beaconAttuale;
     //Istanza dell'attuale percorso calcolato
     private static Percorso percorso;
     //Istanza dell'Adapter Bluetooth
     private static BluetoothAdapter btAdapter;
     //Istanza del gestore Database SQLite
     private static DBHelper dbReference;
+
     public static final int REQUEST_ENABLE_BT = 1;
 
     public static void init(Context context) {
         dbReference = new DBHelper(context);
         initBluetooth(context);
+    }
+
+    public static void getInfoByBeaconID(String beaconAttuale) {
+        edificioAttuale = dbReference.initEdificioAttuale(beaconAttuale);
     }
 
     private static void initBluetooth(Context context) {
