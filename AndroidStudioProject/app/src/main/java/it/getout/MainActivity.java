ccbf36fb2c3ef015096ed0914ec43ed884907323
alessandromentuccia <alessandro.mentuccia@hotmail.it>
2018-02-21 01:08:34 +0100

    creazione classe BluetoothHelper

--- AndroidStudioProject/app/src/main/java/it/getout/MainActivity.java
+++ AndroidStudioProject/app/src/main/java/it/getout/MainActivity.java
@@ -1,11 +1,19 @@
 package it.getout;
 
+import android.bluetooth.BluetoothAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import it.getout.gestioneconnessioni.BluetoothHelper;
 import it.getout.gestioneposizione.PosizioneUtente;
 
 public class MainActivity extends AppCompatActivity {
+
+    //struttura utilizzata per interfacciarsi con il bluetooth
+    private static BluetoothAdapter btAdapter;
+    //scanner per ricercare i dispositivi beacon
+    private static BluetoothHelper btHelper;
+    //scanner per ricercare i dispositivi beacon
 
     private PosizioneUtente posUtente;
 
@@ -15,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         posUtente.init(this);
-    }
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        //creazione del BluetoothHelper
+        if(btAdapter!=null) btHelper = new BluetoothHelper(btAdapter, this);
+        //attivazione del bluetooth (qualora non sia già funzionante)
+        if(!btAdapter.isEnabled()) btHelper.activateBluetooth();
+
+
+}
+
 }
