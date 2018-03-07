f6417ff199881851f4a62917c4853901c8e7f4db
baldo04 <baldo.94@hotmail.it>
2018-03-07 19:19:31 +0100

    cgdg

--- ServerProject/src/connessioni/JsonServer.java
+++ ServerProject/src/connessioni/JsonServer.java
@@ -1,9 +1,14 @@
 package connessioni;
 
 import com.sun.net.httpserver.HttpServer;
+import entita.Edificio;
+import entita.Piano;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
+
+
 
 public class JsonServer {
     private HttpServer server;
@@ -11,7 +16,28 @@ public class JsonServer {
 
     public JsonServer() throws IOException {
         server = HttpServer.create(new InetSocketAddress(9600),0);
-        server.createContext("/edificio",new )
+
+        server.createContext("/edificio",new com.sun.net.httpserver.HttpHandler() {
+            public void handle(com.sun.net.httpserver.HttpExchange arg0) throws IOException {
+
+                String response = Edificio.selectEdificioByBeacon(arg0.getRequestURI().getQuery());  //vuole una stringa e riprende una stringa
+                arg0.sendResponseHeaders(200, response.length());
+                OutputStream os = arg0.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+
+        } );
+
+        server.createContext("/piano", new com.sun.net.httpserver.HttpHandler() {
+            public void handle(com.sun.net.httpserver.HttpExchange arg0) throws IOException {
+                String response = Piano.selectAllPianiByEdificio(arg0.getRequestURI().getQuery());
+                arg0.sendResponseHeaders(200, response.length());
+                OutputStream os = arg0.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        });
     }
 
 }
