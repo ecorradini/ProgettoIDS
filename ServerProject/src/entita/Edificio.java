5c6374cd867ca940ff5c0ca124c17ab54b8a9bee
ecorradini <birillo3000#>
2018-03-07 17:31:43 +0100

    ciaociaociao

---
+++ ServerProject/src/entita/Edificio.java
@@ -0,0 +1,12 @@
+package entita;
+
+public class Edificio {
+    //Nome della colonna "NOME"
+    private static final String NOME = "NOME";
+    //Nome della tabella "EDIFICIO"
+    private static final String TABLE_EDIFICIO = "EDIFICIO";
+
+    public static String selectEdificioByBeacon(String idBeacon) {
+
+    }
+}
