0cfe12e5c6d884e52283b2fbca605985d431f2ee
baldo04 <baldo.94@hotmail.it>
2018-03-07 18:27:11 +0100

    connessioni: databaseconnection jsonserver server

--- ServerProject/src/connessioni/JsonServer.java
+++ ServerProject/src/connessioni/JsonServer.java
@@ -1,4 +1,17 @@
 package connessioni;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class JsonServer {
+    private HttpServer server;
+    private Thread worker;
+
+    public JsonServer() throws IOException {
+        server = HttpServer.create(new InetSocketAddress(9600),0);
+        server.createContext("/edificio",new )
+    }
+
 }
