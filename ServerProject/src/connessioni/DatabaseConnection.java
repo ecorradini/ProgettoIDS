f9648b76d7a8491328112caa6e9595f821666804
ecorradini <birillo3000#>
2018-03-09 10:16:14 +0100

    server

--- ServerProject/src/connessioni/DatabaseConnection.java
+++ ServerProject/src/connessioni/DatabaseConnection.java
@@ -1,6 +1,7 @@
 package connessioni;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,28 +9,17 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static Connection connection;
-    public static final String dbUrl="jdbc:mysql://172.23.171.125:3306/getout";
+    public static final String dbUrl="jdbc:mysql://localhost:3306/getout";
 
     public static void init() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
 
-        connection = DriverManager.getConnection(dbUrl,"root","getout2018");
-        if(hostAvailabilityCheck()) {
-            System.out.println("Connesso al DB");
-        }
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection(dbUrl, "root", "getout2018");
+        System.out.println("Connesso al DB");
 
     }
 
     public static Connection getConn() {
         return connection;
-    }
-
-    public static boolean hostAvailabilityCheck() {
-        try (Socket s = new Socket(dbUrl, 3306)) {
-            return true;
-        } catch (IOException ex) {
-            /* ignore */
-        }
-        return false;
     }
 }
