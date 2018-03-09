1c58d4c02307624bdf8b6da02d0ae0cb12330dff
ecorradini <birillo3000#>
2018-03-09 09:45:04 +0100

    server

--- ServerProject/src/connessioni/DatabaseConnection.java
+++ ServerProject/src/connessioni/DatabaseConnection.java
@@ -1,18 +1,20 @@
 package connessioni;
 
+import java.io.IOException;
+import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static Connection connection;
-    public static final String dbUrl="jdbc:mysql://localhost:3306/getout";
+    public static final String dbUrl="jdbc:mysql://172.23.171.125:3306/getout";
 
     public static void init() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
 
         connection = DriverManager.getConnection(dbUrl,"root","getout2018");
-        if(!connection.isClosed()) {
+        if(hostAvailabilityCheck()) {
             System.out.println("Connesso al DB");
         }
 
@@ -20,5 +22,14 @@ public class DatabaseConnection {
 
     public static Connection getConn() {
         return connection;
+    }
+
+    public static boolean hostAvailabilityCheck() {
+        try (Socket s = new Socket(dbUrl, 3306)) {
+            return true;
+        } catch (IOException ex) {
+            /* ignore */
+        }
+        return false;
     }
 }
--- ServerProject/src/connessioni/Server.java
+++ ServerProject/src/connessioni/Server.java
@@ -8,15 +8,18 @@ public class Server {
     public static void main(String[] args) throws IOException{
 
         try {
+            System.out.println("Server Start");
             DatabaseConnection.init();
 
         } catch (ClassNotFoundException e) {
+            System.out.println("Eccezione Class not found");
             e.printStackTrace();
         } catch (SQLException e) {
+            System.out.println("Eccezione SQL");
             e.printStackTrace();
         }
 
-        JsonServer server = new JsonServer();
+        new JsonServer();
 
     }
 }
