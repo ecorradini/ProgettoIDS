212a48057e553d091a1ba0afb4419d60b987e2fb
Enrico Corradini <enrico.corradini94@gmail.com>
2018-04-11 15:03:40 +0200

    ciao

--- Manuali Utente/server_setup.sh
+++ Manuali Utente/server_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DISTRO=`lsb_release -i | cut -f 2-`
-if [DISTRO="Ubuntu"] 
+if [$DISTRO="Ubuntu"] 
 then
 	wget http://dev.mysql.com/get/mysql-apt-config_0.8.9-1_all.deb
 	dpkg -i mysql-apt-config_0.8.9-1_all.deb
@@ -10,14 +10,14 @@ then
 	mysql_secure_installation
 	mysql_install_db
 	service mysqld start
-elif [DISTRO="CentOS"] 
+elif [$DISTRO="CentOS"] 
 then
 	wget http://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
 	yum -y localinstall mysql57-community-release-el7-11.noarch.rpm
 	rm mysql57-community-release-el7-11.noarch.rpm
 	yum  -y install mysql-community-server
 	service mysqld start
-elif [DISTRO="Fedora"] 
+elif [$DISTRO="Fedora"] 
 then
 	wget http://dev.mysql.com/get/mysql57-community-release-fc27-10.noarch.rpm
 	dnf -y localinstall mysql57-community-release-fc27-10.noarch.rpm
