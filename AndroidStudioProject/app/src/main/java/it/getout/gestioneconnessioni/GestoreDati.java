18a2dd07aa40ee27c0da6db091c628ca70a4f20e
alessandromentuccia <alessandro.mentuccia@hotmail.it>
2018-05-03 17:23:03 +0200

    ddd

--- AndroidStudioProject/app/src/main/java/it/getout/gestioneconnessioni/GestoreDati.java
+++ AndroidStudioProject/app/src/main/java/it/getout/gestioneconnessioni/GestoreDati.java
@@ -1,10 +1,28 @@
 package it.getout.gestioneconnessioni;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import it.getout.gestioneposizione.Aula;
+import it.getout.gestioneposizione.Beacon;
 import it.getout.gestioneposizione.Edificio;
+import it.getout.gestioneposizione.Piano;
+import it.getout.gestioneposizione.Tronco;
 
 public abstract class GestoreDati {
 
     public abstract Edificio richiediEdificioAttuale(String beacon);
 
-    public abstract Edificio richiediEdificioAttuale(String beacon);
+    public abstract Piano richiediPianoAttuale(String edificio);
+
+    public abstract String richiediMappaPiano(Piano piano);
+
+    public abstract HashMap<String,Beacon> richiediBeaconTronco(int idTronco);
+
+    public abstract ArrayList<Tronco> richiediTronchiPiano(String piano);
+
+    public abstract ArrayList<Aula> richiediAulePiano(String piano);
+
+    public abstract ArrayList<Piano> richiediPianiEdificio(String edificio);
+
 }
