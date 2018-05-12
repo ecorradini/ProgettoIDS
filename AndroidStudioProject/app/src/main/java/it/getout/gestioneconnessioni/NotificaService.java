49471cee9c7769625658a38dddb75202d28b3d48
Enrico Corradini <enrico.corradini94@gmail.com>
2018-05-12 11:04:58 +0200

    UT4

--- AndroidStudioProject/app/src/main/java/it/getout/gestioneconnessioni/NotificaService.java
+++ AndroidStudioProject/app/src/main/java/it/getout/gestioneconnessioni/NotificaService.java
@@ -8,7 +8,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.IBinder;
+import android.os.Looper;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -29,8 +31,6 @@ public class NotificaService extends Service {
     private final int UPDATE_INTERVAL = 60 * 1000;
     private Timer timer = new Timer();
     private static final int NOTIFICATION_EX = 1;
-    private NotificationManager notificationManager;
-    private String message;
 
     public Server server;
     private Context context;
@@ -68,6 +68,8 @@ public class NotificaService extends Service {
         Thread inizioNotifica = new Thread() {
             public void run() {
 
+                Looper.prepare();
+
 
                 try {
                     d = new DatagramSocket(9601, InetAddress.getByName("0.0.0.0"));
@@ -83,14 +85,11 @@ public class NotificaService extends Service {
 
                     //Check if the message is correct
                     //QUESTA è DA MODIFICARE PER LEGGERE IL JSON CHE FARà PARTIRE EMERGENZA
-                    message = new String(receivePacket.getData()).trim();
+                    String message = new String(receivePacket.getData()).trim();
+
+                    creaNotifica(message);
 
                     Log.e("message", message);
-
-                    /*if (!message.equals("GETOUT EMERGENZA A: Ingegneria")) {
-                        //DO SOMETHING WITH THE SERVER'S IP (for example, store it in your controller)
-                        message[0] = null;
-                    }*/
                     //Close the port!
                     d.close();
 
@@ -99,132 +98,62 @@ public class NotificaService extends Service {
                 }catch (IOException ex) {
                     Log.i("IP","problema nel ricercare server");
                 }
-
-                //return message;
-
-            /*
-                try {
-                    Thread.sleep(10000);
-                    try {
-                        d = new DatagramSocket(8080, InetAddress.getByName("0.0.0.0"));
-                    } catch (SocketException e) {
-                        e.printStackTrace();
-                    } catch (UnknownHostException e) {
-                        e.printStackTrace();
-                    }
-                    //Wait for a response
-                    byte[] recvBuf = new byte[15];
-                    DatagramPacket receivePacket = new DatagramPacket(recvBuf, recvBuf.length);
-
-                    Log.e("edo3", "edo3");
-
-                    try {
-
-                        d.receive(receivePacket);
-                        Log.e("try","try");
-                    } catch (IOException e) {
-
-                        e.printStackTrace();
-                        Log.e("try54","try54");
-                    }
-
-                    Log.e("edo4", "edo4");
-
-                    //Check if the message is correct
-                    //QUESTA è DA MODIFICARE PER LEGGERE IL JSON CHE FARà PARTIRE EMERGENZA
-                    String m = new String(receivePacket.getData()).trim();
-
-                    message[0] = m;
-                    Log.e("message", m);
-
-                    if (!m.equals("GETOUT EMERGENZA A: Ingegneria")) {
-                        //DO SOMETHING WITH THE SERVER'S IP (for example, store it in your controller)
-                        m = null;
-                    }
-                    //Close the port!
-                    d.close();
-
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            */
             }
         };
-
         inizioNotifica.start();
-
-
-        //try {
-        //    inizioNotifica.join();
-        //}catch (InterruptedException e){
-        //    e.printStackTrace();
-        //}
 
 
         Log.i("edo2","edo2");
 
-
-
-
-
         //creazione notifica funziona !!!
 
-        notificationManager = (NotificationManager)
-        getSystemService(Context.NOTIFICATION_SERVICE);
+        return START_STICKY;
+    }
 
+    private void creaNotifica(String message) {
+        createNotificationChannel();
 
-        Intent actionIntent = new Intent(context, Client.class);
-
-        PendingIntent pending =
-                PendingIntent.getActivity(
-                        context,
-                        0,
-                        actionIntent,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-
+        Intent intent = new Intent(this,Client.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
 
         NotificationCompat.Builder builder = null;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             builder =
-                    new NotificationCompat.Builder(context, NotificationChannel.DEFAULT_CHANNEL_ID)
+                    new NotificationCompat.Builder(context, "getout")
                             .setSmallIcon(R.mipmap.ic_launcher)
-                            .setContentTitle(message)
-                            .setContentIntent(pending)
+                            .setContentTitle("GetOut")
+                            .setContentText(message)
+                            .setContentIntent(pendingIntent)
+                            .setAutoCancel(true)
                             .setPriority(NotificationCompat.PRIORITY_DEFAULT);
         }
         else {
             builder =
                     new NotificationCompat.Builder(context)
                             .setSmallIcon(R.mipmap.ic_launcher)
-                            .setContentTitle(message)
-                            .setContentIntent(pending)
-                            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                            .setContentTitle("GetOut")
+                            .setContentText(message)
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                            .setContentIntent(pendingIntent)
+                            .setAutoCancel(true);
         }
 
-
-
-        Intent notificationIntent = new Intent(this, Client.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-                notificationIntent, 0);
-
-       notificationManager.notify(NOTIFICATION_EX, builder.build());
-        Toast.makeText(this, "Started!", Toast.LENGTH_LONG);
-        timer.scheduleAtFixedRate(new TimerTask() {
-
-            @Override
-            public void run() {
-                // Check if there are updates here and notify if true
-            }
-        }, 0, UPDATE_INTERVAL);
-        return START_STICKY;
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(NOTIFICATION_EX, builder.build());
     }
 
-
-    private void stopService() {
-        if (timer != null) timer.cancel();
+    //Solo per Android >= 8.0
+    private void createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "GetOut";
+            String description = "emergenza";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("getout",name,importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
-
 
 }
