4c7394fcd5bbdf9e195fc8038c2200150e4294a3
baldo04 <baldo.94@hotmail.it>
2018-05-22 12:47:53 +0200

    notifica di inizio e fine client

--- AndroidStudioProject/app/src/main/java/it/getout/gestioneconnessioni/Notifica.java
+++ AndroidStudioProject/app/src/main/java/it/getout/gestioneconnessioni/Notifica.java
@@ -95,16 +95,31 @@ public class Notifica extends Service {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "getout")
-                            .setSmallIcon(R.mipmap.ic_launcher)
-                            .setContentText("Evacuare immediatamente.")
-                            .setContentTitle(message)
-                            .setContentIntent(pendingIntent)
-                            .setAutoCancel(true)
-                            .setPriority(NotificationCompat.PRIORITY_MAX);
+        //controllo sulla stringa di arrivo per creare il messaggio sulla notifica
+        if (message.substring(0,1).equals("E")) {
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "getout")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentText("Evacuare immediatamente.")
+                    .setContentTitle(message)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+                    .setPriority(NotificationCompat.PRIORITY_MAX);
 
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        notificationManager.notify(NOTIFICATION_EX, builder.build());
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            notificationManager.notify(NOTIFICATION_EX, builder.build());
+        }
+        else{
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "getout")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentText("Keep Calm, Emergenza Rientrata")
+                    .setContentTitle(message)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+                    .setPriority(NotificationCompat.PRIORITY_MAX);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            notificationManager.notify(NOTIFICATION_EX, builder.build());
+        }
     }
 
     private void createNotificationChannel() {
