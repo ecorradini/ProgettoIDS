dc64e272de836853528ce26737a3e2e590a6f7cd
baldo04 <baldo.94@hotmail.it>
2018-05-24 17:10:15 +0200

    dump

---
+++ Dumpdb/getoutdb_dbo_AULA.sql
@@ -0,0 +1,5 @@
+INSERT INTO getoutdb.dbo.AULA (NOME, X, Y, PIANO, ENTRATA) VALUES ('1', 12, 11, '160', null);
+INSERT INTO getoutdb.dbo.AULA (NOME, X, Y, PIANO, ENTRATA) VALUES ('140/3', 922, 614, '140', '140/3');
+INSERT INTO getoutdb.dbo.AULA (NOME, X, Y, PIANO, ENTRATA) VALUES ('140/D4', 779, 608, '140', '140/D4');
+INSERT INTO getoutdb.dbo.AULA (NOME, X, Y, PIANO, ENTRATA) VALUES ('2', 1609, 243, '160', 'entrataprova1602');
+INSERT INTO getoutdb.dbo.AULA (NOME, X, Y, PIANO, ENTRATA) VALUES ('edoardo', 1210, 425, '140', 'prova');
---
+++ Dumpdb/getoutdb_dbo_PIANO.sql
@@ -0,0 +1,6 @@
+INSERT INTO getoutdb.dbo.PIANO (NOME, EDIFICIO) VALUES ('140', 'Ingegneria');
+INSERT INTO getoutdb.dbo.PIANO (NOME, EDIFICIO) VALUES ('145', 'Ingegneria');
+INSERT INTO getoutdb.dbo.PIANO (NOME, EDIFICIO) VALUES ('150', 'Ingegneria');
+INSERT INTO getoutdb.dbo.PIANO (NOME, EDIFICIO) VALUES ('160', 'Ingegneria');
+INSERT INTO getoutdb.dbo.PIANO (NOME, EDIFICIO) VALUES ('165', 'Ingegneria');
+INSERT INTO getoutdb.dbo.PIANO (NOME, EDIFICIO) VALUES ('170', 'Ingegneria');
