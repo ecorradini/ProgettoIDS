e0c2172b0c6d98a33547b6db9cb3d472be7c832a
baldo04 <baldo.94@hotmail.it>
2018-05-29 12:18:51 +0200

    download iniziale2

--- ServerProject/src/entita/DAOMappa.java
+++ ServerProject/src/entita/DAOMappa.java
@@ -2,10 +2,15 @@ package entita;
 
 import connessioni.Database;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Base64;
 
 public class DAOMappa {
     static final String PIANO = "PIANO";
@@ -14,23 +19,22 @@ public class DAOMappa {
 
 
     //scaricamento di tutte le mappe per ottenimento dati offline EDO
-    public static String downloadMappe(){
-        String json="MAPPE:[";
+    public static String downloadMappe() {
+        String json = "MAPPE:[";
         try {
             Connection con = Database.getConn();
             Statement stm = con.createStatement();
             ResultSet rs = stm.executeQuery("SELECT * FROM MAPPA");
-            while (rs.next()){
-                json =json+ rs.getString(1)+" "+rs.getString(2)+",";
+            while (rs.next()) {
+                json = json + rs.getString(1) + " " + rs.getString(2) + ",";
             }
             rs.close();
             stm.close();
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        }
-        finally {
-            if(json.substring(json.length() - 1,json.length()).equals(",")) {
+        } finally {
+            if (json.substring(json.length() - 1, json.length()).equals(",")) {
                 json = json.substring(0, json.length() - 1);
             }
             json = json + "]";
@@ -41,17 +45,17 @@ public class DAOMappa {
 
     public static String selectMappaByPiano(String piano) {
         Connection conn = Database.getConn();
-        String link="";
+        String link = "";
 
-        String query =  "SELECT "+ LINK+
-                " FROM "+TABLE_MAPPA+
-                " WHERE "+PIANO+"=\'"+piano+"\'";
+        String query = "SELECT " + LINK +
+                " FROM " + TABLE_MAPPA +
+                " WHERE " + PIANO + "=\'" + piano + "\'";
 
         try {
             Statement stm = conn.createStatement();
             ResultSet rs = stm.executeQuery(query);
 
-            while(rs.next()) {
+            while (rs.next()) {
                 link = rs.getString(LINK);
             }
 
@@ -74,5 +78,26 @@ public class DAOMappa {
 
         stm.close();
     }
+
+    private static String encodeFileToBase64Binary(File file) {
+        String encodedfile = null;
+        try {
+            FileInputStream fileInputStreamReader = new FileInputStream(file);
+            byte[] bytes = new byte[(int) file.length()];
+            fileInputStreamReader.read(bytes);
+            encodedfile = Base64.getEncoder().encodeToString(bytes);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return encodedfile;
+
+
+    }
+
 }
 
