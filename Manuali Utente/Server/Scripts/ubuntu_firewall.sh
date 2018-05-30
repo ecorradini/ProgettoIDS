895394c413e2bbc0bcefcc9624bd4a86d390741f
Pitsi <carloni.jacopo@hotmail.it>
2018-05-30 13:15:21 +0200

    Aggiunto script iptables

---
+++ Manuali Utente/Server/Scripts/ubuntu_firewall.sh
@@ -0,0 +1,21 @@
+# !/bin/bash
+
+# È NECESSARIO ESEGUIRE QUESTO SCRIPT COME ROOT ( $ sudo <nome_script>) 
+
+
+echo "[*] Avvio configurazione Firewall..."
+
+#	iptables -F
+#	iptables -X	
+
+
+iptables --policy INPUT DROP
+iptables --policy OUTPUT DROP
+iptables --policy FORWARD DROP
+
+#	Enable traffic through port 9605 used for DISCOVERY IP
+
+echo "[*] Apertura porta 9605 per funzionalità di DiscoveryIP..."
+
+iptables -A INPUT -p udp --dport 9605 -j ACCEPT
+iptables -A OUTPUT -p udp --dport 9605 -j ACCEPT
